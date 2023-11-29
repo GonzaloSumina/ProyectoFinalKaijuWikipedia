ebe027a281515873c6af851079fcdfdfc1c06d67
ZeroDyna <93954219+ZeroDyna@users.noreply.github.com>
2023-11-29 16:09:16 -0500

    Add files via upload

--- ProyectoFinalKaijuWikipedia/Vistas/Kaiju/detalles.xaml.cs
+++ ProyectoFinalKaijuWikipedia/Vistas/Kaiju/detalles.xaml.cs
@@ -32,11 +32,13 @@ namespace ProyectoFinalKaijuWikipedia.Vistas.Kaiju {
             {
                 // Desliza el botón hacia la derecha al hacer clic por primera vez
                 await recopilarButton.TranslateTo(200, 0, 1000, Easing.SpringOut);
+                recopilarButton.BackgroundColor = Color.Green;
             }
             else
             {
                 // Desliza el botón hacia la izquierda al hacer clic nuevamente
                 await recopilarButton.TranslateTo(0, 0, 1000, Easing.SpringOut);
+                recopilarButton.BackgroundColor = Color.DarkRed;
             }
 
             // Cambia el estado de la variable
