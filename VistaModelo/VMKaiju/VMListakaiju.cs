23d2e360c1db8968ee1438516842d808f219fb0a
GonzaloSumina <gonzalo.sumina@ucsp.edu.pe>
2023-11-29 10:36:55 -0500

    Add files via upload

---
+++ VistaModelo/VMKaiju/VMListakaiju.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using ProyectoFinalKaijuWikipedia.Vistas.Kaiju;
+using ProyectoFinalKaijuWikipedia.Datos;
+using ProyectoFinalKaijuWikipedia.Modelo;
+
+namespace ProyectoFinalKaijuWikipedia.VistaModelo.VMKaiju
+{
+    public class VMListaKaiju: BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        List<MKaiju> _Listakaiju;
+        #endregion
+        #region CONSTRUCTOR
+        public VMListaKaiju(INavigation navigation)
+        {
+            Navigation = navigation;
+            Mostrarkaiju();
+        }
+        #endregion
+        #region OBJETOS
+        public List<MKaiju> Listakaiju 
+        { 
+            get { return _Listakaiju; }
+            set { SetValue(ref _Listakaiju, value); }
+        }
+        #endregion
+        #region PROCESOS    
+        public async Task Mostrarkaiju()
+        {
+            var funcion = new Dkaiju();
+            Listakaiju = await funcion.Mostrarkaiju();
+        }
+
+        public async Task Iraregistro()
+        {
+            await Navigation.PushAsync(new Registrarkaiju());
+        }
+
+        public void ProcesoSimple()
+        {
+
+        }
+        
+        public async Task iradetalle(MKaiju parametros)
+        {
+            await Navigation.PushAsync(new detalles(parametros));
+        }
+
+        #endregion
+        #region COMANDOS    
+        public ICommand Iraregistrocommand => new Command(async () => await Iraregistro());
+        public ICommand iradetallecommand => new Command<MKaiju>(async(p)=> await iradetalle(p));
+        #endregion
+    }
+}
---
+++ Vistas/Kaiju/Registrarkaiju.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ProyectoFinalKaijuWikipedia.Vistas.Kaiju;
+using ProyectoFinalKaijuWikipedia.VistaModelo.VMKaiju;
+
+namespace ProyectoFinalKaijuWikipedia.Vistas.Kaiju
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registrarkaiju : ContentPage
+    {
+        public Registrarkaiju()
+        {
+            InitializeComponent();
+            BindingContext = new VMRegistroKaiju(Navigation);
+        }
+    }
+}
